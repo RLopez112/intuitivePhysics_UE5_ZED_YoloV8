c957e9b67834d30f72dccf2088c20b0beaa3129c
RLopez112 <rodrigo.f.lopez.v@gmail.com>
2023-05-28 16:13:50 -0300

    punto en centro de detección

--- depth_sensing.py
+++ depth_sensing.py
@@ -62,22 +62,24 @@ res = sl.Resolution()
 res.width = 720
 res.height = 404
 
-image = sl.Mat()
-depth = sl.Mat()
 
-camera_model = zed.get_camera_information().camera_model
+
 mirror_ref = sl.Transform()
 mirror_ref.set_translation(sl.Translation(2.75,4.0,0))
 tr_np = mirror_ref.m
 
 # Create OpenGL viewer // for pointcloud display.
-
+#camera_model = zed.get_camera_information().camera_model
 #viewer = gl.GLViewer()
 #viewer.init(len(sys.argv), sys.argv, camera_model, res)
 
-point_cloud = sl.Mat(res.width, res.height, sl.MAT_TYPE.F32_C4, sl.MEM.CPU)
-
 header = ['---', 'boundingBoxLocation', 'boundingBoxRotation', 'boundingBoxScale','SHAPE','fbxPath','pointCloudPath','SimulatePhysicsFromStart?','GravitiAffectsIt?','id','initialSpeedVector']
+
+
+
+
+
+#por ahora solo está devolviendo un objeto
 
 def inferAndMakeBox(source,show):
     results = model.predict(source=source,show = show, conf = 0.8) 
@@ -85,11 +87,12 @@ def inferAndMakeBox(source,show):
     return boxCoordinates
     
 def getBoxCenter(boxCoordinates):
+
     if boxCoordinates.size > 0:
             
-            x_depth=math.trunc(boxCoordinates.item(0))
-            y_depth=math.trunc(boxCoordinates.item(1))
-            z_depth=depth.get_value(x_depth,y_depth)[1]
+            x_depth = math.trunc(boxCoordinates.item(0))
+            y_depth = math.trunc(boxCoordinates.item(1))
+            z_depth = depth.get_value(x_depth,y_depth)[1]
             
             point3D = point_cloud.get_value(x_depth, y_depth)
 
@@ -98,21 +101,27 @@ def getBoxCenter(boxCoordinates):
             z_cloud = point3D[1][2]*100
 
             #print("center",x_cloud,y_cloud,z_depth)
+            location=(x_cloud,y_cloud,z_cloud)
+            location_for_data = "(X="+str(x_cloud)+"Y="+str(y_cloud)+"Z="+str(z_cloud)+")"
 
-            location = "(X="+str(x_cloud)+"Y="+str(y_cloud)+"Z="+str(z_cloud)+")"
-
-            data = ['NewRow',location,'(Pitch=1.261254,Yaw=1.521589,Roll=1.111004)','(X=5.000000,Y=5.000000,Z=5.000000)',1]
-
+            return location
+            
     else:
             print('The array has a size of 0')
 
 
 
-def printFPS():
-    currentFPS = zed.get_current_fps()
-    print(currentFPS)
 
-def writeToCSV(path,header,data):
+def FPS():
+    currentFPS = str(zed.get_current_fps())
+    return currentFPS
+
+
+
+def writeToCSV(path,header,data,location):
+
+    data = ['NewRow',location,'(Pitch=1.261254,Yaw=1.521589,Roll=1.111004)','(X=5.000000,Y=5.000000,Z=5.000000)',1]
+    
     with open(path,mode= 'w', encoding='UTF8') as f:
                 writer = csv.writer(f)
 
@@ -122,22 +131,65 @@ def writeToCSV(path,header,data):
                 # write the data
                 writer.writerow(data)
 
+
+def retrieve_measure_chanels(image,depth,point_cloud):
+    zed.retrieve_image(image, sl.VIEW.LEFT)
+    zed.retrieve_measure(depth, sl.MEASURE.DEPTH)
+    zed.retrieve_measure(point_cloud, sl.MEASURE.XYZRGBA,sl.MEM.CPU, res)
+
+
+def unit_vector(vector):
+     unit_vector = vector / np.linalg.norm(vector)
+     return unit_vector
+
+image = sl.Mat()
+depth = sl.Mat()
+point_cloud = sl.Mat(res.width, res.height, sl.MAT_TYPE.F32_C4, sl.MEM.CPU)
+
 while(True):
 
     if zed.grab() == sl.ERROR_CODE.SUCCESS:
-        
-        zed.retrieve_image(image, sl.VIEW.LEFT)
-        zed.retrieve_measure(depth, sl.MEASURE.DEPTH)
-        zed.retrieve_measure(point_cloud, sl.MEASURE.XYZRGBA,sl.MEM.CPU, res)
-        
-        depth_stream = depth.get_data()
-        
+
+
+        zed.retrieve_image(image, sl.VIEW.LEFT) 
         stream = image.get_data()
+
         image_without_alpha = stream[:,:,:3] #removes alpha chanel      ESTO CUESTA UN MONTÓN!!!!!   VER COMO RESOLVERLO
+        box_coordinate=inferAndMakeBox(image_without_alpha,False)
+        location=getBoxCenter(box_coordinate)
 
-        inferAndMakeBox(image_without_alpha,True)
+        x_depth = 100
+        y_depth = 100
 
-        printFPS()     
+        if box_coordinate.size > 0:
+            x_0 = math.trunc(box_coordinate.item(0)/2)
+            y_0 = math.trunc(box_coordinate.item(1)/2)
+            x_med = math.trunc(box_coordinate.item(2)/2)
+            y_med= math.trunc(box_coordinate.item(3)/2)
+            x_depth = x_0 + x_med
+            y_depth = y_0 + y_med
+
+
+        else:
+            x_depth = 100
+            y_depth = 100
+            print('The array has a size of 0')
+
+        coordinates = (100,100)
+        font = cv2.FONT_HERSHEY_SIMPLEX
+        fontScale = 1
+        color = (255,0,255)
+        thickness = 2
+
+        cv2.circle(stream, (x_depth,y_depth), 5, (0,255,100), -1)
+
+        print(x_depth,y_depth)
+        
+        cv2.imshow('video',stream)
+
+        
+        print(FPS())     
+        
     
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
