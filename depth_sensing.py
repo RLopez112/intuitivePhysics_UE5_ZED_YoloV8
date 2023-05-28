3d48b9cc2f2e96b04633e0d1fa36a4aad922ce90
RLopez112 <rodrigo.f.lopez.v@gmail.com>
2023-05-28 12:32:09 -0300

    dejarlo prolijo para presentación en sociedad. Separé cosas en funciones. No dejé el pipeline hasta guardar csv funcionando.
    Sacar el alpha cuesta un monton. Revisarlo
    ahroa esta funcionando a 4fps.

--- depth_sensing.py
+++ depth_sensing.py
@@ -31,88 +31,118 @@ import numpy as np
 import cv2
 from ultralytics import YOLO
 from PIL import Image
+import csv
+import cv2
 
 model = YOLO("yolov8n-seg.pt")
-#results = model.predict(source="1",show = True, conf = 0.8)
-
-if __name__ == "__main__":
-
-    print("Running Depth Sensing sample ... Press 'Esc' to quit")
-    
-    init = sl.InitParameters(camera_resolution=sl.RESOLUTION.HD720,
-                                 depth_mode=sl.DEPTH_MODE.PERFORMANCE,
-                                 coordinate_units=sl.UNIT.METER,
-                                 coordinate_system=sl.COORDINATE_SYSTEM.RIGHT_HANDED_Y_UP)
-    
-    
-
-    zed = sl.Camera()
-    status = zed.open(init)
-    init.depth_maximum_distance=400
-    #runtime_parameters.sensing_mode = sl.SENSING_MODE.FILL
-    
-    if status != sl.ERROR_CODE.SUCCESS:
-        print(repr(status))
-        exit()
-
-    res = sl.Resolution()
-    res.width = 720
-    res.height = 404
-    
-    image = sl.Mat()
-    depth = sl.Mat()
-    camera_model = zed.get_camera_information().camera_model
-
-    mirror_ref = sl.Transform()
-    mirror_ref.set_translation(sl.Translation(2.75,4.0,0))
-    tr_np = mirror_ref.m
-
-    # Create OpenGL viewer
-    viewer = gl.GLViewer()
-    viewer.init(len(sys.argv), sys.argv, camera_model, res)
-
-    point_cloud = sl.Mat(res.width, res.height, sl.MAT_TYPE.F32_C4, sl.MEM.CPU)
-
   
+
+# define a video capture object
+vid = cv2.VideoCapture(1)
+width = vid.get(cv2.CAP_PROP_FRAME_WIDTH)
+height= vid.get(cv2.CAP_PROP_FRAME_HEIGHT)
+
+print("Running Depth Sensing sample ... Press 'q' to quit")
+    
+init = sl.InitParameters(camera_resolution=sl.RESOLUTION.HD720,
+                             depth_mode=sl.DEPTH_MODE.PERFORMANCE,
+                             coordinate_units=sl.UNIT.METER,
+                             coordinate_system=sl.COORDINATE_SYSTEM.RIGHT_HANDED_Y_UP)
+
+
+zed = sl.Camera()
+status = zed.open(init)
+init.depth_maximum_distance=400
+
+if status != sl.ERROR_CODE.SUCCESS:
+    print(repr(status))
+    exit()
+
+res = sl.Resolution()
+res.width = 720
+res.height = 404
+
+image = sl.Mat()
+depth = sl.Mat()
+
+camera_model = zed.get_camera_information().camera_model
+mirror_ref = sl.Transform()
+mirror_ref.set_translation(sl.Translation(2.75,4.0,0))
+tr_np = mirror_ref.m
+
+# Create OpenGL viewer // for pointcloud display.
+
+#viewer = gl.GLViewer()
+#viewer.init(len(sys.argv), sys.argv, camera_model, res)
+
+point_cloud = sl.Mat(res.width, res.height, sl.MAT_TYPE.F32_C4, sl.MEM.CPU)
+
+header = ['---', 'boundingBoxLocation', 'boundingBoxRotation', 'boundingBoxScale','SHAPE','fbxPath','pointCloudPath','SimulatePhysicsFromStart?','GravitiAffectsIt?','id','initialSpeedVector']
+
+def inferAndMakeBox(source,show):
+    results = model.predict(source=source,show = show, conf = 0.8) 
+    boxCoordinates=results[0].boxes.xyxy.numpy()
+    return boxCoordinates
+    
+def getBoxCenter(boxCoordinates):
+    if boxCoordinates.size > 0:
             
-
-
-    while viewer.is_available():
-        if zed.grab() == sl.ERROR_CODE.SUCCESS:
-            zed.retrieve_image(image, sl.VIEW.LEFT)
-            # Retrieve depth map. Depth is aligned on the left image
-            zed.retrieve_measure(depth, sl.MEASURE.DEPTH)
-            zed.retrieve_measure(point_cloud, sl.MEASURE.XYZRGBA,sl.MEM.CPU, res)
-            #viewer.updateData(point_cloud)
-
-            cv2.imshow("ZED", image.get_data())
-            #key = cv2.waitKey(5)
-
+            x_depth=math.trunc(boxCoordinates.item(0))
+            y_depth=math.trunc(boxCoordinates.item(1))
+            z_depth=depth.get_value(x_depth,y_depth)[1]
             
-            
+            point3D = point_cloud.get_value(x_depth, y_depth)
 
-            x = round(image.get_width() / 2)
-            y = round(image.get_height() / 2)
-            err, point_cloud_value = point_cloud.get_value(x, y)
-            
-            distance = math.sqrt(point_cloud_value[0] * point_cloud_value[0] +
-                                 point_cloud_value[1] * point_cloud_value[1] +
-                                 point_cloud_value[2] * point_cloud_value[2])
+            x_cloud = point3D[1][0]*100
+            y_cloud = point3D[1][1]*100
+            z_cloud = point3D[1][2]*100
 
-            point_cloud_np = point_cloud.get_data()
-            point_cloud_np.dot(tr_np)
+            #print("center",x_cloud,y_cloud,z_depth)
 
-            if not np.isnan(distance) and not np.isinf(distance):
-                #print("Distance to Camera at ({}, {}) (image center): {:1.3} m".format(x, y, distance), end="\r")
-                print(depth.get_value(x,y))
-                # Increment the loop
-                
-            else:
-                print("Can't estimate distance at this position.")
-                print("Your camera is probably too close to the scene, please move it backwards.\n")
-                
-            sys.stdout.flush()
+            location = "(X="+str(x_cloud)+"Y="+str(y_cloud)+"Z="+str(z_cloud)+")"
+
+            data = ['NewRow',location,'(Pitch=1.261254,Yaw=1.521589,Roll=1.111004)','(X=5.000000,Y=5.000000,Z=5.000000)',1]
+
+    else:
+            print('The array has a size of 0')
 
 
-    viewer.exit()
-    zed.close()
+
+def printFPS():
+    currentFPS = zed.get_current_fps()
+    print(currentFPS)
+
+def writeToCSV(path,header,data):
+    with open(path,mode= 'w', encoding='UTF8') as f:
+                writer = csv.writer(f)
+
+                # write the header
+                writer.writerow(header)
+
+                # write the data
+                writer.writerow(data)
+
+while(True):
+
+    if zed.grab() == sl.ERROR_CODE.SUCCESS:
+        
+        zed.retrieve_image(image, sl.VIEW.LEFT)
+        zed.retrieve_measure(depth, sl.MEASURE.DEPTH)
+        zed.retrieve_measure(point_cloud, sl.MEASURE.XYZRGBA,sl.MEM.CPU, res)
+        
+        depth_stream = depth.get_data()
+        
+        stream = image.get_data()
+        image_without_alpha = stream[:,:,:3] #removes alpha chanel      ESTO CUESTA UN MONTÓN!!!!!   VER COMO RESOLVERLO
+
+        inferAndMakeBox(image_without_alpha,True)
+
+        printFPS()     
+    
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+  
+# After the loop release the cap object
+vid.release()
+# Destroy all the windows
+cv2.destroyAllWindows()
